9e47b7189864e6625482f34cccccb131fa5f5cd5
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2025-06-09 16:19:49 +1200

    weird things with the grid culling
    turned on depth clipping but still behaving weird

--- ui/sceneview.py
+++ ui/sceneview.py
@@ -14,7 +14,7 @@ _last_pan_x = 0.0
 _last_pan_y = 0.0
 _last_rot_dx = 0.0
 _last_rot_dy = 0.0
-camera_z = 100.0
+camera_z = 600.0
 
 def create_sceneview(parent):
     init_input_handlers()
@@ -23,12 +23,14 @@ def create_sceneview(parent):
         with dpg.drawlist(parent=parent, width=width, height=height, tag="sceneview_drawlist"):
             with dpg.draw_layer(
                 tag="sceneview_layer",
-                depth_clipping=False,
+                depth_clipping=True,
                 perspective_divide=True,
                 cull_mode=dpg.mvCullMode_Back
             ):
                 with dpg.draw_node(tag="sceneview_node"):
-                    pass
+                    draw_grid()
+                    draw_axes()
+                    # draw_stl(os.path.join("assets", "models", "drone.stl"), "sceneview_node")
         draw_scene()
 
     dpg.set_frame_callback(1, finish_setup)
@@ -37,7 +39,8 @@ def on_scroll(sender, app_data):
     if dpg.is_item_hovered("sceneview_drawlist"):
         global camera_z
         camera_z -= app_data * camera_z/10
-        camera_z = max(10, min(500, camera_z))
+        camera_z = max(400, min(1400, camera_z))
+        print(f"Camera Z: {camera_z}")
         draw_scene()
 
 def pan_callback(sender, app_data):
@@ -77,7 +80,7 @@ def draw_scene():
     w = dpg.get_item_width("sceneview_drawlist")
     h = dpg.get_item_height("sceneview_drawlist")
 
-    proj = dpg.create_perspective_matrix(fov=math.radians(90), aspect=w/h, zNear=0.1, zFar=1000)
+    proj = dpg.create_perspective_matrix(fov=math.radians(90), aspect=w/h, zNear=100, zFar=2500)
     view = dpg.create_fps_matrix([pan_offset[0], pan_offset[1], camera_z], pitch=0.0, yaw=0.0)
 
     model = (
@@ -85,27 +88,23 @@ def draw_scene():
         dpg.create_rotation_matrix(math.pi*y_rot/180.0, [0, 0, 1])    # yaw (around Z)
     )
 
-
-
     transform = proj * view * model
 
-    dpg.set_clip_space("sceneview_layer", 0, 0, w, h, -1.0, 1.0)
+    dpg.set_clip_space("sceneview_layer", 0, 0, w, h, 0.0, 1.0)
     dpg.apply_transform("sceneview_node", transform)
 
-    draw_grid()
-    draw_axes()
-    draw_stl(os.path.join("assets", "models", "drone.stl"), "sceneview_node")
-
 def draw_grid():
-    spacing = 20
+    spacing = 10
     count = 20
     half = spacing * count // 2
     gray = (100, 100, 100, 255)
+    z_offset = -0.1  # Push grid slightly below 0 to avoid z-fighting
 
     for i in range(-count // 2, count // 2 + 1):
         x = i * spacing
-        dpg.draw_line(( x, -half, 0.0), ( x,  half, 0.0), color=gray, parent="sceneview_node")
-        dpg.draw_line((-half,  x, 0.0), ( half,  x, 0.0), color=gray, parent="sceneview_node")
+        dpg.draw_line(( x, -half, z_offset), ( x,  half, z_offset), color=gray, parent="sceneview_node")
+        dpg.draw_line((-half,  x, z_offset), ( half,  x, z_offset), color=gray, parent="sceneview_node")
+
 
 def draw_axes():
     origin = (0.0, 0.0, 0.0)
