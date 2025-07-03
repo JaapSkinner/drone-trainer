ed9d6f6523fbb21ecc7424a65a3b36760f39ee5e
Jaap Skinner <jaap.skinner@hotmail.co.nz>
2025-07-04 10:35:24 +1200

    exploded the app folder

--- ui/main_window.py
+++ ui/main_window.py
@@ -2,10 +2,10 @@ import pygame
 from PyQt5.QtCore import QTimer, Qt, pyqtSlot
 from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QPushButton, QWidget
 
-from app.services.joystick_service import JoystickService
-from app.ui.gl_widget.gl_widget import GLWidget
-from app.ui.dock.dock_manager import create_dock
-from app.models.structs import PositionData
+from services.joystick_service import JoystickService
+from ui.gl_widget.gl_widget import GLWidget
+from ui.dock.dock_manager import create_dock
+from models.structs import PositionData
 
 class MainWindow(QMainWindow):
     def __init__(self, vicon, parent=None):
